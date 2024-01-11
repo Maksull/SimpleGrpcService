6dc455f28cfc8507dc53fb666b6663e1a410a930
Maksull <kyrkyrkyr99@gmail.com>
2024-01-11 20:48:59 +0200

    Implement basic entities

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -1,6 +1,31 @@
-﻿namespace Domain.Entities;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using ProtoBuf;
 
+namespace Domain.Entities;
+
+[ProtoContract]
 public sealed class Category
 {
-    
+    private string _categoryId = string.Empty;
+
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    [ProtoMember(1)]
+    public required string CategoryId
+    {
+        get => _categoryId;
+        set
+        {
+            if (!Ulid.TryParse(value, out _))
+            {
+                throw new ArgumentException("Invalid ULID format");
+            }
+
+            _categoryId = value;
+        }
+    }
+
+    [ProtoMember(2)] 
+    public required string Name { get; set; }
 }
--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -1,6 +1,33 @@
-﻿namespace Domain.Entities;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using ProtoBuf;
 
+namespace Domain.Entities;
+
+[ProtoContract]
 public sealed class Product
 {
+    private string _productId = string.Empty;
     
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    [ProtoMember(1)]
+    public required string ProductId {
+        get => _productId;
+        set
+        {
+            if (!Ulid.TryParse(value, out _))
+            {
+                throw new ArgumentException("Invalid ULID format");
+            }
+
+            _productId = value;
+        }
+    }
+    [ProtoMember(2)]
+    public string Name { get; set; } = string.Empty;
+    [ProtoMember(3)]
+    public string Description { get; set; } = string.Empty;
+    [ProtoMember(4)]
+    public required string CategoryId { get; set; }
 }
