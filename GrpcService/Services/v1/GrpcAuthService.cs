b34db30f5388c24fede70ee36f933e4f06227324
Maksull <kyrkyrkyr99@gmail.com>
2024-01-27 20:02:12 +0200

    Add simple auth

---
+++ GrpcService/Services/v1/GrpcAuthService.cs
@@ -0,0 +1,38 @@
+﻿using Application.Mediatr.Commands.Auth;
+using Grpc.Core;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using SimpleGrpcProject.v1;
+
+namespace GrpcService.Services.v1;
+
+public sealed class GrpcAuthService : AuthServiceProto.AuthServiceProtoBase
+{
+    private readonly IMediator _mediator;
+
+    public GrpcAuthService(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public override async Task<LoginResponse> Login(LoginRequest request, ServerCallContext context)
+    {
+        var jwt = (await _mediator.Send(new LoginCommand(request.Login, request.Password), context.CancellationToken) ?? "");
+
+        return new()
+        {
+            Jwt = jwt
+        };
+    }
+
+    [Authorize]
+    public override Task<TestAuthResponse> TestAuth(TestAuthRequest request, ServerCallContext context)
+    {
+        TestAuthResponse t = new()
+        {
+            Response = request.Message + " RESPONSE MESSAGE"
+        };
+
+        return Task.FromResult(t);
+    }
+}
